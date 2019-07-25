e799e96a981a395a8396db49e55bdee4232f7bea
Administrator <Administrator@192.168.1.103>
2019-07-25 22:58:49 +0800

    通过输出,将所有的编码输入到某个文件下

--- src/com/book/book_18/_010/AvailableCharSets.java
+++ src/com/book/book_18/_010/AvailableCharSets.java
@@ -1,18 +1,26 @@
 package com.book.book_18._010;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.util.Map;
 
 public class AvailableCharSets {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		FileOutputStream outputStream = new FileOutputStream(TestFc.fileName);
+		FileChannel out = outputStream.getChannel();
 		for(Map.Entry<String,Charset> charset : Charset.availableCharsets().entrySet()) {
-			
 			Charset sonCharset = charset.getValue();
 			StringBuilder toString = new StringBuilder(charset.getKey()).append(" : ");
 			for(String aliases : sonCharset.aliases()) {
 				toString.append(aliases).append(" ");
 			}
 			System.out.println(toString);
+			out.write(ByteBuffer.wrap(toString.append("\n").toString().getBytes(System.getProperty("file.encoding"))));
 		}
+		out.close();
+		outputStream.close();
 	}
 }
